9e29d1dde5e7b25b0f8b8c921ad6aa87f8f11c91
sumit-singh-mehra <singhmehrasumit@gmail.com>
2023-04-17 14:07:01 +0530

    UI updated

---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+// library imports
+import { PlusIcon } from "@heroicons/react/24/solid";
+
+const CustomForm = ({ addTask }) => {
+  const [task, setTask] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addTask({
+      name: task,
+      checked: false,
+      id: Date.now(),
+    });
+    setTask("");
+  };
+
+  return (
+    <form className="todo" onSubmit={handleFormSubmit}>
+      {/* input form and label */}
+      <div className="wrapper">
+        <input
+          type="text"
+          id="task"
+          className="input"
+          value={task}
+          onInput={(e) => setTask(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label htmlFor="task" className="label">
+          Enter Task
+        </label>
+      </div>
+
+      {/* Add button */}
+      <button className="btn" aria-label="Add Task" type="submit">
+        <PlusIcon />
+      </button>
+    </form>
+  );
+};
+export default CustomForm;
